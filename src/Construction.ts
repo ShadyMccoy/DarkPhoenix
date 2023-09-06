d2d6af58abb0b05cb31fdee805875ee81f305d58
ShadyMccoy <reiniroosh+git@gmail.com>
2023-09-05 21:12:53 -0700

    add Abstract class RoomProgram to be extended by specific routines

--- src/Construction.ts
+++ src/Construction.ts
@@ -1,109 +1,72 @@
 import { ConstructionSiteStruct } from "ConstructionSite";
-import { any, forEach, sortBy } from "lodash";
+import { RoomRoutine } from "RoomProgram";
+import { any, forEach } from "lodash";
 
-export function construction(room: Room) {
-    console.log('construction');
+export class Construction extends RoomRoutine {
+    name = "construction";
+    constructionSite!: ConstructionSiteStruct;
 
-    //calculateConstructionSites(room);
-
-    if (!room.memory.constructionSites) { room.memory.constructionSites = [] as ConstructionSiteStruct[]; }
-
-    let sites = room.memory.constructionSites as ConstructionSiteStruct[];
-    sites = _.filter(sites, (site) => {
-        return Game.getObjectById(site.id) != null;
-    });
-
-    if (sites.length == 0) {
-        let s = room.find(FIND_MY_CONSTRUCTION_SITES);
-        if (s.length == 0) { return; }
-
-        room.memory.constructionSites.push({ id: s[0].id, Builders: [] as Id<Creep>[] });
+    constructor() {
+        super();
     }
 
-    if (sites.length == 0) { return; }
+    routine(room: Room): void {
+        console.log('construction');
 
-    forEach(sites, (s) => {
-        RemoveDeadCreeps(s);
-        AddNewlySpawnedCreeps(s, room);
-        SpawnBuilderCreep(s, room);
-        BuildConstructionSite(s);
-    });
+        //calculateConstructionSites(room);
 
-    room.memory.constructionSites = sites;
-}
+        if (!room.memory.constructionSites) { room.memory.constructionSites = [] as ConstructionSiteStruct[]; }
 
-function RemoveDeadCreeps(site: ConstructionSiteStruct) {
-    site.Builders = _.filter(site.Builders, (builder) => {
-        return Game.getObjectById(builder) != null;
-    });
-}
-
-function AddNewlySpawnedCreeps(site: ConstructionSiteStruct, room: Room): void {
-    if (site.Builders.length == 0) {
-        let idleBuilders = room.find(FIND_MY_CREEPS, {
-            filter: (creep) => {
-                return creep.memory.role == "builder" && !creep.spawning;
-            }
+        let sites = room.memory.constructionSites as ConstructionSiteStruct[];
+        sites = _.filter(sites, (site) => {
+            return Game.getObjectById(site.id) != null;
         });
 
-        if (idleBuilders.length == 0) { return }
+        if (sites.length == 0) {
+            let s = room.find(FIND_MY_CONSTRUCTION_SITES);
+            if (s.length == 0) { return; }
 
-        let ConstructionSite = Game.getObjectById(site.id)!;
-
-        let idleBuilder = sortBy(idleBuilders, (creep) => {
-            return creep.pos.getRangeTo(ConstructionSite.pos);
-        })[0];
-
-        site.Builders.push(idleBuilder.id);
-
-        idleBuilder.memory.role = "busyBuilder";
-    }
-}
-
-function SpawnBuilderCreep(
-    site: ConstructionSiteStruct,
-    room: Room): boolean {
-    if (site.Builders.length < 1) {
-
-        let spawns = room.find(FIND_MY_SPAWNS);
-        let ConstructionSite = Game.getObjectById(site.id)!;
-        spawns = sortBy(spawns, s => s.pos.findPathTo(ConstructionSite.pos).length);
-        let spawn = spawns[0];
-
-        return spawn.spawnCreep(
-            [WORK, WORK, CARRY, MOVE],
-            spawn.name + Game.time,
-            { memory: { role: "builder" } }) == OK;
-    }
-
-    return false;
-}
-
-function BuildConstructionSite(site: ConstructionSiteStruct) {
-    let ConstructionSite = Game.getObjectById(site.id)!;
-    let builders = site.Builders.map((builder) => {
-        return Game.getObjectById(builder)!;
-    });
-
-    if (builders.length == 0) { return; }
-    let builder = builders[0];
-
-    if (builder.pos.getRangeTo(ConstructionSite.pos) > 3) {
-        builder.moveTo(ConstructionSite.pos);
-    } else {
-        builder.build(ConstructionSite);
-    }
-}
-
-function calculateConstructionSites(room: Room) {
-    let constructionSites = room.find(FIND_MY_CONSTRUCTION_SITES);
-    forEach(constructionSites, (site) => {
-        if (!any(room.memory.constructionSites, (s) => { return s.id == site.id })) {
-            let newSite = {
-                id: site.id,
-                Builders: [] as Id<Creep>[]
-            } as ConstructionSiteStruct;
-            room.memory.constructionSites.push(newSite);
+            room.memory.constructionSites.push({ id: s[0].id, Builders: [] as Id<Creep>[] });
         }
-    });
+
+        if (sites.length == 0) { return; }
+
+        forEach(sites, (s) => {
+            this.RemoveDeadCreeps();
+            this.AddNewlySpawnedCreeps(room);
+            this.SpawnCreeps(room);
+            this.BuildConstructionSite(s);
+        });
+
+        room.memory.constructionSites = sites;
+    }
+
+    BuildConstructionSite(site: ConstructionSiteStruct) {
+        let ConstructionSite = Game.getObjectById(site.id)!;
+        let builders = site.Builders.map((builder) => {
+            return Game.getObjectById(builder)!;
+        });
+
+        if (builders.length == 0) { return; }
+        let builder = builders[0];
+
+        if (builder.pos.getRangeTo(ConstructionSite.pos) > 3) {
+            builder.moveTo(ConstructionSite.pos);
+        } else {
+            builder.build(ConstructionSite);
+        }
+    }
+
+    calculateConstructionSites(room: Room) {
+        let constructionSites = room.find(FIND_MY_CONSTRUCTION_SITES);
+        forEach(constructionSites, (site) => {
+            if (!any(room.memory.constructionSites, (s) => { return s.id == site.id })) {
+                let newSite = {
+                    id: site.id,
+                    Builders: [] as Id<Creep>[]
+                } as ConstructionSiteStruct;
+                room.memory.constructionSites.push(newSite);
+            }
+        });
+    }
 }
--- src/RoomProgram.ts
+++ src/RoomProgram.ts
@@ -1,35 +1,87 @@
-import { forEach } from "lodash";
+import { forEach, keys, sortBy } from "lodash";
 import { bootstrap } from "bootstrap";
 import { energyMining } from "EnergyMining";
 import { energyCarrying } from "EnergyCarrying";
-import { construction } from "Construction";
+import { Construction } from "Construction";
 
 export function RoomProgram(room: Room) {
   forEach(getRoomRoutines(room), (routine) => {
-    switch (routine) {
-      case "bootstrap":
-        bootstrap(room);
-        break;
-      case "energyMining":
-        energyMining(room);
-        break;
-      case "energyCarrying":
-        energyCarrying(room);
-        break;
-      case "construction":
-        construction(room);
-        break;
-      default:
-        console.log(`Routine '${routine}' not found.`);
-    }
+    routine.RemoveDeadCreeps();
+    routine.AddNewlySpawnedCreeps(room);
+    routine.SpawnCreeps(room);
+    routine.routine(room);
   });
 }
 
-function getRoomRoutines(room: Room): string[] {
+function getRoomRoutines(room: Room): RoomRoutine[] {
   if (room.controller?.level == 1) {
-    return ["construction", "energyCarrying", "energyMining", "bootstrap"];
+    return [new Construction()]; //, "energyCarrying", "energyMining", "bootstrap"];
   } else {
-    return ["energyMining"];
+    return [];
+  }
+}
+
+export abstract class RoomRoutine {
+  name!: string;
+  position!: RoomPosition;
+
+  spawnQueue!:
+    {
+      body: BodyPartConstant[],
+      pos: RoomPosition,
+      role: string
+    }[];
+
+  creepIds!: {
+    [role: string]: Id<Creep>[];
+  };
+
+  abstract routine(room: Room): void;
+
+  RemoveDeadCreeps(): void {
+    forEach(keys(this.creepIds), (role) => {
+      this.creepIds[role] = _.filter(this.creepIds[role], (creepId: Id<Creep>) => {
+        return Game.getObjectById(creepId) != null;
+      });
+    });
+  }
+
+  AddNewlySpawnedCreeps(room: Room): void {
+    forEach(keys(this.creepIds), (role) => {
+      if (this.spawnQueue.length) return;
+
+      let idleCreeps = room.find(FIND_MY_CREEPS, {
+        filter: (creep) => {
+          return creep.memory.role == role && !creep.spawning;
+        }
+      });
+
+      if (idleCreeps.length == 0) { return }
+
+
+      let closestIdleCreep = sortBy(idleCreeps, (creep) => {
+        return creep.pos.getRangeTo(this.position);
+      })[0];
+
+      this.creepIds[role].push(closestIdleCreep.id);
+
+      closestIdleCreep.memory.role = "busy" + role;
+    });
+  }
+
+  SpawnCreeps(room: Room): void {
+      if (this.spawnQueue.length == 0) return;
+
+      let spawns = room.find(FIND_MY_SPAWNS, { filter: spawn => !spawn.spawning });
+      if (spawns.length == 0) return;
+
+      spawns = sortBy(spawns, spawn => this.position.findPathTo(spawn).length);
+      let spawn = spawns[0];
+
+      spawn.spawnCreep(
+        this.spawnQueue[0].body,
+        spawn.name + Game.time,
+        { memory: { role: this.spawnQueue[0].role } }) == OK;
   }
 }
 
